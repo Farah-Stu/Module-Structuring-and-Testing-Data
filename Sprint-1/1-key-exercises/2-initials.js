7c6b50dc78990a4ccfbf81d450c26c0e5cde5079
Farah-Stu <selma.salam45@gmail.xom>
2025-06-14 15:54:05 +0100

    declaring initials with first character of each string

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -1,3 +1,6 @@
+
+
+
 let firstName = "Creola";
 let middleName = "Katherine";
 let lastName = "Johnson";
@@ -5,7 +8,14 @@ let lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-let initials = ``;
+let initials=
+`${firstName[0]}${middleName[0]}${lastName[0]}`;
+
+console.log(initials);
+
+
+//I declared the variable called initials rhen I used [0] to store the first character of each string
+
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
 
